1335d475ff8b000d8790df6ee632ebcee8b40fe2
SMHRD <SMHRD@DESKTOP-GMJDOGD>
2020-06-22 09:36:07 +0900

    ㅇㅇ

--- JavaTeamProject/src/gitTest/Main.java
+++ JavaTeamProject/src/gitTest/Main.java
@@ -6,14 +6,11 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("ÇÏÀ×,,?");
 		
-<<<<<<< HEAD
+
 		System.out.println("test12");
-=======
+
 		System.out.println("È£À×,,?");
-		
-		System.out.println("¿Ö?");
->>>>>>> branch 'master' of https://github.com/2020-SMHRD-BIGDATA1/Public-Facilities-Reservation.git
-		
+
 	}
 
 }
