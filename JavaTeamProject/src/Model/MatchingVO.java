9b0dce96c8ede9b2cd17385528cb997a5b03aea0
이지훈 <59809529+jihoon2723@users.noreply.github.com>
2020-06-22 17:56:45 +0900

    ㅠㅠ

--- JavaTeamProject/src/Model/MatchingVO.java
+++ JavaTeamProject/src/Model/MatchingVO.java
@@ -1,5 +1,48 @@
 package Model;
 
-public class MatchingVO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class MatchingVO {
+	private Connection conn;
+	private PreparedStatement pst;
+	private ResultSet rs;
+
+	private void getConnection() {
+
+		// 1.JDBC드라이버 동적로딩
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "hr";
+			String password = "hr";
+			// 2.DB연결
+			conn = DriverManager.getConnection(url, user, password);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void close() {
+		// 6.DB연결종료
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pst != null) {
+				pst.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
