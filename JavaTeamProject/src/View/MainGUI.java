8fc8d551681316b032fc27f5f0cda8443bb9f62e
SMHRD <SMHRD@DESKTOP-GMJDOGD>
2020-06-23 14:04:51 +0900

    commit

---
+++ JavaTeamProject/src/View/MainGUI.java
@@ -0,0 +1,45 @@
+package View;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MainGUI {
+
+	private JFrame frame;
+	
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainGUI window = new MainGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 360, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
